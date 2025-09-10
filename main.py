755c946312d0681efa9cf931890926720b058c9d
Mason Acevedo <mason.acevedo2000@gmail.com>
2025-09-10 15:40:57 -0700

    simple control scheme - alternative left-right between frames!

--- main.py
+++ main.py
@@ -7,11 +7,17 @@ print(f"Starting observation: {observation}")
 episode_over = False
 total_reward = 0
 
+count = 0
 while not episode_over:
-    action = env.action_space.sample()
+    action = count % 2
+    count += 1
+    
+    # print(f"Action: {action}")
+    # print("type(action): ", type(action))
     observation, reward, terminated, truncated, info = env.step(action)
+    # print(f"Observation: {observation}")
     total_reward += reward
     episode_over = terminated or truncated
-
+    
 print(f"Episode finished! Total reward: {total_reward}")
 env.close()
