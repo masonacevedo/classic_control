90a463f3b88f0e78e6f1baf8e16265c0a627b0da
Mason Acevedo <mason.acevedo2000@gmail.com>
2026-04-09 11:53:07 -0700

    add function for running episode and getting back rewards

--- main.py
+++ main.py
@@ -25,23 +25,29 @@ def evaluateAgent(agentToTest, numEpisodes=10, showRender=False):
         total_reward_across_episodes += episode_reward
     return total_reward_across_episodes/numEpisodes
 
-currentParams = [np.random.uniform(-1,1) for _ in range(0,4)]
-currentEvaluation = evaluateAgent(Agent(currentParams))
-lastPrint = time.time()
-n=400
-for i in range(0, n):
-    if (time.time() - lastPrint) > 10:
-        lastPrint = time.time()
-        print("i/n:",100*i/n)
-        print()
-    adjustments = [np.random.uniform(-0.1, 0.1) for _ in range(0,4)]
-    newParams = [param + adjustment for param, adjustment in zip(currentParams, adjustments)]
-    newEvaluation = evaluateAgent(Agent(newParams), numEpisodes=30)
-    if newEvaluation > currentEvaluation:
-        currentParams = newParams
-        currentEvaluation = newEvaluation
+def runEpisode(agentToUse, showRender=False):
+    if showRender:
+        env = gym.make("CartPole-v1", render_mode="human")
+    else:
+        env = gym.make("CartPole-v1", render_mode=None)
 
-bestAgent = Agent(currentParams)
+    observation, info = env.reset()
+    episode_over = False
+    all_rewards = []
+    while not episode_over:
+        action = agentToUse.chooseAction(observation)
+        observation, reward, terminated, truncated, info = env.step(action)
+        all_rewards.append(reward)
+        episode_over = (terminated or truncated)
+    env.close()
+    return all_rewards
 
-bestReward = evaluateAgent(bestAgent, numEpisodes=1, showRender=True)
-print("bestReward:", bestReward)
+weights = [np.random.uniform(-1,1) for _ in range(0,4)]
+rewards = runEpisode(Agent(weights), showRender=False)
+print("rewards:", rewards)
+print("sum(rewards):", sum(rewards))
+# for each timestep:
+#     compute G_t
+#     plug in state, compute gradient of action we took with respect to learned weights
+#     compute nudge = learning rate * G_t * gradient
+#     weights += nudge
