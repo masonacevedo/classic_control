618e5d9322fb8671e60e105c530408a84e7bc8fa
Mason Acevedo <mason.acevedo2000@gmail.com>
2026-04-09 12:21:40 -0700

    agent now samples from probability distribution to determine action

--- agent.py
+++ agent.py
@@ -1,19 +1,33 @@
+import numpy as np
+
 class Agent:
     def __init__(self, params):
         if len(params) != 4:
             raise Exception("Invalid argument to agent construtor! Must be a list of 4 parameters.")
         self.params = params
 
-    def chooseAction(self, observation):
+    def chooseAction(self, observation, verbose=False):
         w,x,y,z = observation
         a,b,c,d = self.params
 
-        calculation = a*w + b*x + c*y + d*z
+        score = a*w + b*x + c*y + d*z
+        p = 1/(1+np.exp(-1*score))
 
-        if calculation > 0:
-            return 1
+        sample = np.random.uniform(0,1)
+
+        if p < sample:
+            action = 0
         else:
-            return 0
+            action = 1
+
+        if verbose:
+            print("score:", score)
+            print("p:", p)
+            print("sample:", sample)
+            print("action:", action)
+            print()
+        return action
+
 
     def __repr__(self):
         return str([self.a, self.b, self.c, self.d])
