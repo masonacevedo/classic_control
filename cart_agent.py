393f73a469e320ec6513f71aedc4a954c816a19e
Mason Acevedo <mason.acevedo2000@gmail.com>
2026-04-10 10:37:47 -0700

    refactor cart agent to use torch

--- cart_agent.py
+++ cart_agent.py
@@ -1,43 +1,24 @@
 import numpy as np
 
-class CartAgent:
-    def __init__(self, params):
-        if len(params) != 6:
-            raise Exception("Invalid argument to agent construtor! Must be a list of 6 parameters.")
-        self.params = params
+import torch
+import torch.nn as nn
+
+class CartAgent(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.linear = nn.Linear(2,3, bias=False)
 
     def chooseAction(self, observation, verbose=False):
+        with torch.no_grad():
+            probabilities = self.forward(observation)
+        return torch.multinomial(probabilities, 1).item()
 
-        probabilities = self.calcActionProbabilities(observation)
-
-        sample = np.random.uniform(0,1)
-        if sample < probabilities[0]:
-            action = 0
-        elif sample < probabilities[0] + probabilities[1]:
-            action = 1
-        else:
-            action = 2
-
-        if verbose:
-            print("probabilities:", probabilities)
-            print("sample:", sample)
-            print("action:", action)
-            print()
-        return action
-
-    def calcActionProbabilities(self, observation):
-        x,y = observation
-        a1, b1, a2, b2, a3, b3 = self.params
-
-        score1 = a1*x + b1*y
-        score2 = a2*x + b2*y
-        score3 = a3*x + b3*y
+    def forward(self, observation):
+        x = torch.tensor(observation)
+        logits = self.linear(x)
+        probabilities = torch.softmax(logits, dim=0)
+        return probabilities
         
-        totalExp = np.exp(score1) + np.exp(score2) + np.exp(score3)
-        return [np.exp(score1)/totalExp, np.exp(score2)/totalExp, np.exp(score3)/totalExp]
-
-    def updateWeights(self, nudge):
-        self.params = [num + param for num, param in zip(nudge, self.params)]
 
     def __repr__(self):
         return str(self.params)
