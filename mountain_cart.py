4d707f9789c3549d6d7718ab9ed414f447f3a43c
Mason Acevedo <mason.acevedo2000@gmail.com>
2026-04-10 00:02:10 -0700

    mountain cart refactor

--- mountain_cart.py
+++ mountain_cart.py
@@ -2,16 +2,29 @@ import gymnasium as gym
 from cart_agent import CartAgent
 import numpy as np
 
-env = gym.make("MountainCar", render_mode="human")
+
+
+def runEpisode(agentToUse, env):
+    observation, info = env.reset()
+    episode_over = False
+
+    states = [observation]
+    actions = []
+    rewards = []
+    while not(episode_over):
+        action = agentToUse.chooseAction(observation)
+        observation, reward, terminated, truncated, info = env.step(action)
+        states.append(observation)
+        actions.append(action)
+        rewards.append(reward)
+        episode_over = (terminated or truncated)
+
+    env.close()
+
+    return states, actions, rewards
+
 
 params = [np.random.uniform(-1,1) for _ in range(0,6)]
 agent = CartAgent(params)
-
-observation, info = env.reset()
-episode_over = False
-
-while not(episode_over):
-    action = agent.chooseAction(observation, verbose=True)
-    observation, time_step_reward, terminated, truncated, info = env.step(action)
-    episode_over = (terminated or truncated)
-env.close()
+env = gym.make("MountainCar", render_mode="human")
+states, actions, rewards = runEpisode(agent, env)
