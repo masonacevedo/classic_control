551511dcd41be3149584c24ad76971c8e7298846
Mason Acevedo <mason.acevedo2000@gmail.com>
2026-04-20 19:00:31 -0700

    initial TicTacToeBot class

---
+++ TicTacToeBot.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn as nn
+
+class TicTacToeBot(nn.Module):
+    def __init__(self):
+        super().__init__()
+        # shared trunk
+        self.linear1 = nn.Linear(18, 20, bias=True)
+        self.linear2 = nn.Linear(20, 20, bias=True)
+
+        # value head
+        self.value_head = nn.linear(20, 1, bias=True)
+
+        # poilcy head
+        self.policy_head = nn.Linear(20, 9, bias=True)
+    
+    def forward(self, x):
+        x = torch.tensor(x)
+        
+        x = self.linear1(x)
+        x = torch.relu(x)
+        
+        x = self.linear2(x)
+        x = torch.relu(x)
+
+        value = self.value_head(x)
+        value = torch.relu(value)
+
+        policy = self.policy_head(x)
+        policy = torch.relu(policy)
+
+        return value, policy
+        
+        
